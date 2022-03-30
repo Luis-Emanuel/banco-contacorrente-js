e48354622425d7d529b71be27a7265d1700ca97c
Luis-Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2022-03-30 19:43:08 -0300

    Funções cliente e conta corrente

---
+++ ContaCorrente.js
@@ -0,0 +1,23 @@
+export class ContaCorrente{
+    agencia;
+    cliente;
+
+    #saldo = 0;
+
+    sacar(valor){
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+            return valor;
+        };
+    };
+
+    depositar(valor){
+        if(valor <= 0) return;
+        this.#saldo += valor;  
+    };
+
+    transferir(valor, conta){
+        const valorsacado = this.sacar(valor);
+        conta.depositar(valorsacado)
+    }
+};
