1998d34180ab4a050db65b1ed4b774e5dd6d55e3
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2022-05-25 14:20:19 -0300

    Autentificado de senha

--- Cliente.js
+++ Cliente.js
@@ -1,9 +1,13 @@
 export class Cliente{
     //CONSTRUTOR
-    constructor(nome, cpf){
+    constructor(nome, cpf, senha){
         this.nome = nome;
         this._cpf = cpf;
+        this._senha = senha
     };
+    autenticar(){
+        return true
+    }
     //ACESSORES
     get cpf(){
         return this._cpf;
--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import { Cliente } from "./Cliente.js";
 import { Gerente } from "./Funcionarios/Gerente.js";
 import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutentificacao.js";
 
+const diretor = new Diretor(12345678900, "Ronaldo", 10000)
+const gerente = new Gerente(12345678901, "Adalton", 5000)
+const cliente = new Cliente("Emanuel", 123456678902, "789")
+diretor.cadastraSenha("123");
+gerente.cadastraSenha("456");
+
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "456");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "789")
+
+
+console.log(diretorEstaLogado);  
+console.log(gerenteEstaLogado);
+console.log(clienteEstaLogado);
